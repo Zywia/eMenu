c07c72281cf71a1ba7728acb81d0e12df9cfbb79
Zywia <39103029+Zywia@users.noreply.github.com>
2021-04-21 09:54:45 +0200

    Update celery.py
    small fix

--- eMenu/eMenu/celery.py
+++ eMenu/eMenu/celery.py
@@ -1,24 +1,18 @@
 import os
 
 from celery import Celery
-# set the default Django settings module for the 'celery' program.
+
 from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eMenu.settings')
 
 app = Celery('eMenu')
 
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    # Executes every Monday morning at 7:30 a.m.
     'test': {
         'task': 'menuCard.tasks.send_email_rapport',
         'schedule': crontab(hour=10, minute=0, day_of_week='*'),
